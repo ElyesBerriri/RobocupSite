2d91d8ea945e414cf9d3146835d85bf10ae00fad
Amine-Staali <121713672+Amine-Staali@users.noreply.github.com>
2023-04-23 01:17:08 +0100

    Profil

--- frontend/src/app/participation/participation.component.ts
+++ frontend/src/app/participation/participation.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-participation',
   templateUrl: './participation.component.html',
   styleUrls: ['./participation.component.css']
 })
-export class ParticipationComponent {
+export class ParticipationComponent  implements OnInit{
+  currentParticipant: any;
 
+  constructor(private ParticipantService: participantService) { }
+//me
+  ngOnInit(): void {
+    this.participantService.getUserBoard().subscribe(
+      data => {
+        this.currentParticipant = this.ParticipantService.getUser();
+      },
+      err => {
+        this.token.signOut();
+        window.location.assign("/home");
+      }
+    );
+
+  }
 }
